03b1483425f57c99582a723027333418b8b9cb4c
ArshdevSingh <arshdevrajput@gmail.com>
2025-06-11 14:54:09 +0530

    conifg fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,6 @@ import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = Lusitana({
-  weight: "700",
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
